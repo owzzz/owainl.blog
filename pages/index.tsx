28bfe191bc025ca77ffedab71ff373629128a215
owain <owain@apple.com>
2022-12-17 11:41:26 -0600

    Mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,22 +12,22 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col h-screen">
-        <header className="bg-white px-4 flex items-center justify-between h-[60px] flex-none">
+        <header className="bg-white px-6 md:px-4 flex items-center justify-between h-[60px] flex-none">
           <Link className="text-sm text-black font-almarai tracking-wide" href="/">OWAINL.co</Link>
         </header>
         <section className="flex justify-center items-center w-full flex-1">
-          <div>
-            <h1 className="text-4xl font-almarai tracking-wide leading-normal">Owain Llewellyn_<br /><span className="text-5xl">Software Engineer &amp; Designer.</span></h1>
+          <div className="px-6 md:px-4">
+            <h1 className="text-2xl md:text-3xl font-almarai tracking-wide leading-normal">Owain Llewellyn_<br /><span className="text-3xl md:text-5xl mt-3 block">Software Engineer &amp; Designer.</span></h1>
             <p className="font-light text-gray-500 mt-4 leading-relaxed">Passionate About Digital Technologies, Great Design & Clever Entrepreneurial Ideas.</p>
-            <div className="flex mt-2 items-center text-gray-500">
+            <div className="flex mt-2 items-center text-gray-500 font-light">
               <p>Building Things At</p> 
-              <Image src="/apple.svg" className="ml-1.5 relative bottom-0.5" alt="Apple Logo" width={20} height={20} />
+              <Image src="/apple.svg" className="ml-2 relative bottom-0.5" alt="Apple Logo" width={20} height={20} />
             </div>
 
-            <p className="text-xs text-black mt-6">Website development in progress...</p>
+            <p className="text-xs text-black mt-6 font-light">Website development in progress...</p>
           </div>
         </section>
-        <footer className="flex-none flex px-4 justify-end items-center h-[50px] bg-gray-100">
+        <footer className="flex-none flex px-6 md:px-4 justify-end items-center h-[50px] bg-gray-100">
           <span className="text-xs text-gray-600">V0.1</span>
         </footer>
       </main>
