e7a209e8678eb1d4aef48827c8c5b98366a9261f
owain <owain@apple.com>
2022-12-28 07:01:31 -0600

    Add copy link

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,6 +5,7 @@ import fs from 'fs';
 import matter from 'gray-matter';
 import { beVietnamPro, roboto } from '../../lib/fonts';
 import { formatDate } from '../../components/post-meta';
+import CopyLink from '../../components/copy-link';
 
 type Page = {
   body: string;
@@ -22,7 +23,11 @@ export default async function About() {
     <header className='border-b border-gray-100'>
       <span className='inline-block text-gray-400 font-semibold uppercase text-xs'>About</span>
       <h1 className={`${beVietnamPro.variable} font-title text-4xl`}>{page.title}</h1>
-      <p className={`${roboto.variable} font-body text-xs py-4 text-gray-400`}>Last updated: {formatDate(page.date)}</p>
+      <div className='flex items-center justify-between pt-4 pb-4'>
+        <p className={`${roboto.variable} font-body text-xs text-gray-400`}>Last updated: {formatDate(page.date)}</p>
+        <CopyLink />
+      </div>
+      
     </header>
     <Markdown html={htmlFromMarkdown} />
   </main>;
