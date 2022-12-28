337a6e9415722b9182bc05640c6d6167d52fc465
owain <owain@apple.com>
2022-12-27 18:45:57 -0600

    update path

--- components/main-nav/index.tsx
+++ components/main-nav/index.tsx
@@ -8,7 +8,7 @@ export default function MainNav() {
             <div className="flex items-center justify-between max-w-6xl w-full">
                 <Link className="text-sm text-black tracking-wide" href="/">OWAINL.co</Link>
                 <ul className='flex space-x-8'>
-                    <li><Link className={`text-xs hover:underline ${usePathname()?.includes('about') ?? 'underline font-bold'}`} href="/about">About</Link></li>
+                    <li><Link className={`text-xs hover:underline ${usePathname()?.includes('about') ? 'underline font-bold' : ''}`} href="/about">About</Link></li>
                     <li><Link className="text-xs hover:underline" href="mailto:owain@owainl.co?subject=Website%20enquiry">Contact</Link></li>
                 </ul>
             </div>
