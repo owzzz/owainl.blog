e7fec7b8331b5106855a35cf089309c1ca21a412
owain <owain@apple.com>
2022-12-28 07:09:37 -0600

    reduce height on mobile

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -22,7 +22,7 @@ export default async function About() {
   return <main className='max-w-[680px] w-full px-6 space-y-6 pb-6'>
     <header className='border-b border-gray-100'>
       <span className='inline-block text-gray-500 font-semibold uppercase text-xs'>About</span>
-      <h1 className={`${beVietnamPro.variable} font-title text-4xl`}>{page.title}</h1>
+      <h1 className={`${beVietnamPro.variable} font-title text-2xl md:text-4xl`}>{page.title}</h1>
       <div className='flex items-center justify-between pt-4 pb-4'>
         <p className={`${roboto.variable} font-body text-sm text-gray-500`}>Last updated: {formatDate(page.date)}</p>
         <CopyLink />
