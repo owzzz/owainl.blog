2dcd35c5b935d70c3da3bf6c769f1bf64ceca4fd
owain <owain@apple.com>
2023-12-18 22:05:59 -0600

    Add @vercel/analytics as a dependency

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
     import "../app.css";
+
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+    
+    inject({ mode: dev ? 'development' : 'production' });
+
     import GlobalNavigation from "$lib/components/global-nav/component.svelte";
     import Footer from "$lib/components/footer/component.svelte";
 
