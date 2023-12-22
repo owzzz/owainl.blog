b75c3a8ff111071217e2acef7559280976d67e75
owain <owain@apple.com>
2023-12-22 14:34:19 -0600

    Add 404 page

---
+++ src/lib/components/centered-content/component.svelte
@@ -0,0 +1,7 @@
+<main class="flex-1 flex flex-col items-center justify-center w-full px-6 space-y-6 pb-6 mt-4 md:mt-8">
+	<section class="flex justify-center items-center w-full max-w-[1220px] flex-1">
+    <div>
+      <slot />
+    </div>
+  </section>
+</main>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+    import CenteredContent from '$lib/components/centered-content/component.svelte';
+</script>
+
+<CenteredContent>
+  <h1 class="font-title text-1xl md:text-2xl tracking-wide leading-normal text-center">
+    Error 404<span class="animate-blink">_</span><br />
+
+    <span class="text-1xl md:text-3xl mt-2 block">
+      This page is unavailable or has been moved.
+    </span>
+  </h1>
+  <p class="font-body text-gray-500 mt-4 leading-relaxed text-center">
+    Please check the URL and try again.
+  </p>
+</CenteredContent>
