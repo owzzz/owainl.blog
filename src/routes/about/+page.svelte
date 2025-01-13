12f9560018d20faeebf09fcd6bfef9f925edb987
owain <owain.llew@gmail.com>
2025-01-12 21:07:49 -0600

    Update copy

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,9 +5,9 @@
             <h1 class="font-title text-3xl md:text-4xl pb-4">Hey, I'm Owain Llewellyn,<br/>a Software Engineer and Designer based in Austin, TX.</h1>
         </header>
         <div class="content">
-          <p>Originally from Wales in the UK, I have been living and working in the states for nearly 10 years, raising two young girls with my <a href="https://www.selostudios.com" target="_blank">wife</a> in Austin, Texas. I am currently building internal tools at Apple, working as a full-stack engineer and moonlighting as UI/UX designer and proto-typer.</p>
+          <p>Originally from Wales in the UK, I have been living and working in the states for nearly 10 years, raising two young girls with my <a href="https://www.selostudios.com" target="_blank">wife</a> in Austin, Texas. I am currently building internal tools at Apple, working as a full-stack engineer and moonlighting as UI/UX designer.</p>
           
-          <p>I am a collaborative and motivated self-starter, proactive in solving complex problems, operating in fast-paced and dynamic environments with experience in building high-quality enterprise software at scale. I have worked with a variety of luxury brands, tech companies, and government organizations across Europe and North America.</p>
+          <p>I am a collaborative and motivated self-starter, proactive in solving technical problems, operating in fast-paced and dynamic environments with experience in building high-quality enterprise software at scale. I have worked with a variety of luxury brands, tech companies, and government organizations across Europe and North America.</p>
           
           <p>In my spare time I enjoy tinkering with technology, reading books, hanging out with family, cycling with friends and cooking outside.</p>
           
