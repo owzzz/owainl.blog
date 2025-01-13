ab800aff6967c21f5b05fc0fd76229b2f709520a
owain <owain.llew@gmail.com>
2025-01-12 22:47:56 -0600

    Update client

--- src/lib/sanity/client.server.ts
+++ src/lib/sanity/client.server.ts
@@ -1,8 +1,14 @@
 import { createClient } from '@sanity/client';
 
+import { NEXT_PUBLIC_SANITY_DATASET, NEXT_PUBLIC_SANITY_PROJECT_ID } from '$env/static/private';
+
+if (!NEXT_PUBLIC_SANITY_PROJECT_ID || !NEXT_PUBLIC_SANITY_DATASET) {
+	throw new Error('Did you forget to run sanity init --env?');
+}
+
 export const client = createClient({
-	projectId: process.env.PUBLIC_SANITY_PROJECT_ID,
-	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+	projectId: NEXT_PUBLIC_SANITY_PROJECT_ID,
+	dataset: NEXT_PUBLIC_SANITY_DATASET,
 	useCdn: false,
 	apiVersion: '2023-11-19',
   perspective: 'published'
