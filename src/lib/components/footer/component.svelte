7fe61bdb4507a87c49b74e64eda7668f09a905be
owain <owain.llew@gmail.com>
2025-01-14 22:33:17 -0600

    Update version number

--- src/lib/components/footer/component.svelte
+++ src/lib/components/footer/component.svelte
@@ -1,3 +1,3 @@
 <footer class="w-full flex-none flex px-6 md:px-4 justify-end items-center h-[50px] bg-gray-100 border-t border-gray-200">
-  <a data-sveltekit-preload-data class="text-xs text-gray-600" href="/roadmap">V0.2.0</a>
+  <a data-sveltekit-preload-data class="text-xs text-gray-600" href="/roadmap">V0.5.0</a>
 </footer>
