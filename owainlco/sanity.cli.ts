b76e2b0e09b12da43a0898a7706e56fac8fbb651
owain <owain.llew@gmail.com>
2025-01-20 08:09:53 -0600

    tweaks to studio

--- owainlco/sanity.cli.ts
+++ owainlco/sanity.cli.ts
@@ -1,6 +1,7 @@
 import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
+  studioHost: 'owainl',
   api: {
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET
