a81631ea07712ed74673a2a521aaf1e9fe189265
owain <owain.llew@gmail.com>
2025-01-30 19:01:12 -0600

    Add speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,19 @@
 <script>
-    import "../app.css";
+  import "../app.css";
 
-    import { dev } from '$app/environment';
-    import { inject } from '@vercel/analytics';
-    
-    inject({ mode: dev ? 'development' : 'production' });
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+  
+  inject({ mode: dev ? 'development' : 'production' });
 
-    import GlobalNavigation from "$lib/components/global-nav/component.svelte";
-    import Footer from "$lib/components/footer/component.svelte";
+  import GlobalNavigation from "$lib/components/global-nav/component.svelte";
+  import Footer from "$lib/components/footer/component.svelte";
 
-    import "@fontsource/roboto";
-    import '@fontsource/be-vietnam-pro/500.css';
+  import "@fontsource/roboto";
+  import '@fontsource/be-vietnam-pro/500.css';
+  import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+  injectSpeedInsights();
 </script>
 
 <GlobalNavigation />
