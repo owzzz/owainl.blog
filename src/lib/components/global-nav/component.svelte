13e43919aadf4df5e3d12ef83264ef02c255ef5b
owain <owain.llew@gmail.com>
2025-01-30 21:36:32 -0600

    Remove notes section

--- src/lib/components/global-nav/component.svelte
+++ src/lib/components/global-nav/component.svelte
@@ -7,7 +7,6 @@
         <a data-sveltekit-preload-data class="text-sm text-black tracking-wide" href="/">OWAINL.co</a>
         <ul class='flex space-x-4 md:space-x-8'>
             <li><a data-sveltekit-preload-data class="text-xs hover:underline {$page.url.pathname === '/about' ? 'font-bold underline' : ''}" href="/about">About</a></li>
-            <li><a data-sveltekit-preload-data class="text-xs hover:underline {$page.url.pathname.startsWith('/notes') ? 'font-bold underline' : ''}" href="/notes">Notes</a></li>
             <li><a data-sveltekit-preload-data class="text-xs hover:underline {$page.url.pathname.startsWith('/books') ? 'font-bold underline' : ''}" href="/books">Books</a></li>
             <li><a class="text-xs hover:underline" href="mailto:owain.llew@gmail.com?subject=Website%20enquiry">Contact</a></li>
         </ul>
