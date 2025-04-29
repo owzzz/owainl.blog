0ac3516d25106af93603c8437ffaa447bce4c936
Owain Llewellyn <owain@apple.com>
2025-04-28 21:25:22 -0500

    Update animations and leading

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,7 @@
 <main class="flex-1 flex flex-col items-center justify-center w-full px-6 space-y-6 pb-6 mt-4 md:mt-8">
     <section class='w-full max-w-[640px]'>
         <header class='border-b border-gray-200 mb-6'>
-            <h1 class="font-title text-3xl md:text-4xl pb-4">Hey, I'm <span class="text-gray-500">Owain Llewellyn</span>,<br/>a Full Stack Engineer &amp; UX Designer based in Austin, TX.</h1>
+            <h1 class="font-title text-3xl md:text-4xl !leading-[41px] md:!leading-[50px] pb-4">Hey, I'm <span class="text-gray-500">Owain Llewellyn</span>,<br/>a Full Stack Engineer &amp; UX Designer based in Austin, TX.</h1>
         </header>
         <div class="content">
           <p>Originally from Wales in the UK, I have been living and working in the states for nearly 10 years, raising two young girls with my <a href="https://www.selostudios.com" target="_blank">wife</a> in Austin, Texas. I am currently building internal tools at Apple, working as a full-stack UI/UX engineer.</p>
